b69c861a0b89bb97788668945064bf03cf6ca547
Halton Bagley <haltonba94@gmail.com>
2020-01-16 20:57:34 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -10,7 +10,7 @@ end
 def print_first_directors_movie_titles
   title_counter = 0
   while title_counter < directors_database[0][:movies].length do
-  puts directors_database[0][:movies][title_counter][:title]
-  title_counter += 1
+    puts directors_database[0][:movies][title_counter][:title]
+    title_counter += 1
   end
 end
